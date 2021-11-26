36b9dd4a1b4051108ebaaabdaf42b7db8a0258ea
Amer Mall <amer@icloud.com>
2021-11-26 13:14:25 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+// Create a variable called random that will generate a random integer greater than 0
+// -- math.random() - This method generates a random number between 0 (inclusive) and 1 (exclusive):
+const random = Math.floor((Math.random() * 10));
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20
+const max = Math.max(5, 10, 15, 20);
